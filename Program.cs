4da6e5fba7c3723398611029a4084767b9f0dde5
Ostwick <henriqueneji@gmail.com>
2024-10-21 23:32:57 -0300

    Fiz a correção do código e resolvi o desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Criando um celular Nokia
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111222333444", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine(); // Quebra de linha para separação
+
+// Criando um celular iPhone
+Smartphone iphone = new Iphone(numero: "987654321", modelo:  "iPhone 14", imei: "555666777888", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
